4f45f10bdbd30e37bc8f0aa8cabdb074d82beb8a
florence martino <florence.martino@lifen.fr>
2022-12-20 20:09:06 +0100

    return PONG

--- app/main.js
+++ app/main.js
@@ -3,10 +3,10 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
-  const test = 'test'
+
+  connection.write('+PONG\r\n');
+
 });
 
-server.listen(6379, "127.0.0.1");
+server.listen(1234, "127.0.0.1");
