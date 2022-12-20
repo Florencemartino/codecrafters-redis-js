c58d3332aebb3f5e9f15800aed6a22e7d768cf4d
florence martino <florence.martino@lifen.fr>
2022-12-20 19:30:51 +0100

    CLI tests (cli-test-1671561051100)

--- app/main.js
+++ app/main.js
@@ -5,8 +5,18 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
-  const test = 'test'
+  // confirm socket connection from client
+  console.log((new Date()) + 'A client connected to server...');
+  socket.on('data', function (connection) {
+    var json = JSON.parse(connection.toString());
+    console.log(json)
+  });
+  // send info to client
+  socket.write('Echo from server: NODE.JS Server \r\n');
+  socket.pipe(socket);
+  socket.end();
+  console.log('The client has disconnected...\n');
+
 });
 
 server.listen(6379, "127.0.0.1");
