03ee673d2776a50c4a568548e8f16dc30fed7e99
florence martino <florence.martino@lifen.fr>
2022-12-20 13:57:59 +0100

    CLI tests (cli-test-1671541079427)

--- app/main.js
+++ app/main.js
@@ -6,7 +6,7 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
-  const test = 'test'
+  return connection
 });
 
 server.listen(6379, "127.0.0.1");
