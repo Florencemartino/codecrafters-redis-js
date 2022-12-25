1a00527c4ce72a491c1853735a859e7188ab80e6
florence martino <florence.martino@lifen.fr>
2022-12-25 18:45:22 +0100

    CLI tests (cli-test-1671990322540)

--- app/main.js
+++ app/main.js
@@ -3,10 +3,17 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-const server = net.createServer((connection) => {
+var server = net.createServer(function (c) { //'connection' listener
+  console.log('client connected');
 
-  connection.write('+PONG\r\n');
+  console.log(JSON.stringify(c))
 
+  c.on('end', function () {
+    console.log('client disconnected');
+  });
+
+  c.write('+PONG\r\n');
+  // c.pipe(c);
 });
 
 server.listen(6379, "127.0.0.1");
