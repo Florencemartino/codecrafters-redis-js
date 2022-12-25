4878628bf8e0b5b9a44dad36ed1ea5bbfcc9fb68
florence martino <florence.martino@lifen.fr>
2022-12-25 19:22:36 +0100

    CLI tests (cli-test-1671992556689)

--- app/main.js
+++ app/main.js
@@ -5,7 +5,29 @@ console.log("Logs from your program will appear here!");
 
 const server = net.createServer((connection) => {
 
-  connection.write('+PONG\r\n');
+
+
+    const socket = net.createConnection(6379, "127.0.0.1");
+    console.log('Socket created.');
+    socket.on('data', function (data) {
+      // Log the response from the HTTP server.
+      console.log('RESPONSE: ' + data);
+    }).on('connect', function () {
+      // Manually write an HTTP request.
+      socket.write("GET / HTTP/1.0\r\n\r\n");
+    }).on('end', function () {
+      console.log('DONE');
+    });
+    connection.write('+PONG\r\n');
+    console.log('connected to server!');
+    client.write('world!\r\n');
+  });
+
+  // while (client) {
+  //   connection.write('+PONG\r\n');
+  // }
+
+
 
 });
 
