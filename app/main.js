e2a00cfa8e4a8e7a4d3ecca1a4758310e807bb32
florence martino <florence.martino@lifen.fr>
2022-12-26 18:10:46 +0100

    CLI tests (cli-test-1672074646222)

--- app/main.js
+++ app/main.js
@@ -5,7 +5,27 @@ console.log("Logs from your program will appear here!");
 
 const server = net.createServer((connection) => {
 
-  connection.write('+PONG\r\n');
+  let client;
+
+  if (!client) {
+    client = net.createConnection(6379, "127.0.0.1", () => {
+    console.log('Socket created.');
+      // 'connect' listener.
+
+    });
+  }
+
+
+  client.on('data', (data) => {
+
+    connection.write('+PONG\r\n');
+    console.log(data.toString());
+    client.end();
+  });
+
+  client.on('end', () => {
+    console.log('disconnected from server');
+  });
 
 });
 
