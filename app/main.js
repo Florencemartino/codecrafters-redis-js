f51a0c331adc8889eb901d107176eb8c05b33e18
florence martino <florence.martino@lifen.fr>
2023-01-08 11:25:05 +0100

    stage 6 ok

--- app/main.js
+++ app/main.js
@@ -3,23 +3,43 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+const hash = {}
+
 const server = net.createServer((connection) => {
 
   console.log('New Client connected')
 
   connection.on('data', data => {
 
-    const test = data.toString()
+    const bufferToString = data.toString()
 
-    const word = test.split('\r\n')[4]
+    const command = bufferToString.split('\r\n')[2]
 
-    if (!word) {
+    if (command.toLowerCase() === 'ping') {
       return connection.write('+PONG\r\n')
     }
 
-    const wordRest = `+` + word + `\r\n`
+    if (command.toLowerCase() === 'echo') {
+      const word = bufferToString.split('\r\n')[4]
+      const wordRest = `+` + word + `\r\n`
+      return connection.write(wordRest)
+    }
 
-    return connection.write(wordRest)
+    if (command.toLowerCase() === 'set') {
+      const key = bufferToString.split('\r\n')[4]
+      const value = bufferToString.split('\r\n')[6]
+
+      hash[key] = value
+
+      return connection.write('+OK\r\n')
+    }
+
+    if (command.toLowerCase() === 'get') {
+      const key = bufferToString.split('\r\n')[4]
+
+      return connection.write(`+${hash[key]}\r\n`)
+    }
+
   })
 
   connection.on('end', () => {
